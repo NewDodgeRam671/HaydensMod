8412806ca9c6ee09da114a7cf67846b3c19f1300
NewDodgeRam671 <hayden.rheinfelder@gmail.com>
2022-07-06 17:33:11 -0700

    comment

--- src/main/java/net/newdodgeram/haydensmod/HaydensMod.java
+++ src/main/java/net/newdodgeram/haydensmod/HaydensMod.java
@@ -1,16 +1,17 @@
 package net.newdodgeram.haydensmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.newdodgeram.haydensmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-\\comment
 public class HaydensMod implements ModInitializer {
 	public static final String MOD_ID = "haydensmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+
+		ModItems.registerModItems();
 
 	}
 }
---
+++ src/main/java/net/newdodgeram/haydensmod/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.newdodgeram.haydensmod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.newdodgeram.haydensmod.HaydensMod;
+
+public class ModItems {
+
+    public static final Item GOLEM_HEART = registerItem("golem_heart",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item GOLEMS_BLOOM = registerItem("golems_bloom",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(HaydensMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        HaydensMod.LOGGER.debug("Registering Mod Items for " + HaydensMod.MOD_ID);
+    }
+}
