d0e8989440c3416572e16ff2a89362e44f843362
NewDodgeRam671 <hayden.rheinfelder@gmail.com>
2022-07-12 19:25:32 -0700

    Added Wyrmwood

--- src/main/java/net/newdodgeram/haydensmod/block/ModBlocks.java
+++ src/main/java/net/newdodgeram/haydensmod/block/ModBlocks.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
+import net.minecraft.block.PillarBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -19,13 +20,27 @@ import net.newdodgeram.haydensmod.item.ModItemGroup;
 public class ModBlocks {
     public static final Block HAYDONIUM_BLOCK = registerBlock("haydonium_block",
             new Block(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroup.GOLEMS);
+
     public static final Block JUMPY_BLOCK = registerBlock("jumpy_block",
             new JumpyBlock(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroup.GOLEMS);
+
     public static final Block ORNATE_LAMP = registerBlock("ornate_lamp",
             new OrnateLampBlock(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()
                     .luminance(state -> state.get(OrnateLampBlock.LIT) ? 15 : 0)), ModItemGroup.GOLEMS);
+
     public static final Block EGGPLANT_CROP = registerBlockWithoutItem("eggplant_crop",
             new EggplantCropBlock(FabricBlockSettings.copy(Blocks.WHEAT)));
+
+    public static final Block WYRMWOOD_LOG = registerBlock("wyrmwood_log",
+            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_LOG).strength(4f).requiresTool()), ModItemGroup.GOLEMS);
+    public static final Block WYRMWOOD_WOOD = registerBlock("wyrmwood_wood",
+            new PillarBlock(FabricBlockSettings.copy(Blocks.OAK_WOOD).strength(4f).requiresTool()), ModItemGroup.GOLEMS);
+    public static final Block STRIPPED_WYRMWOOD_LOG = registerBlock("stripped_wyrmwood_log",
+            new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_LOG).strength(4f).requiresTool()), ModItemGroup.GOLEMS);
+    public static final Block STRIPPED_WYRMWOOD_WOOD = registerBlock("stripped_wyrmwood_wood",
+            new PillarBlock(FabricBlockSettings.copy(Blocks.STRIPPED_OAK_WOOD).strength(4f).requiresTool()), ModItemGroup.GOLEMS);
+    public static final Block WYRMWOOD_PLANKS = registerBlock("wyrmwood_planks",
+            new Block(FabricBlockSettings.copy(Blocks.OAK_PLANKS).strength(4f).requiresTool()), ModItemGroup.GOLEMS);
 
     private static Block registerBlock(String name, Block block, ItemGroup tab) {
         registerBlockItem(name, block, tab);
--- src/main/java/net/newdodgeram/haydensmod/util/ModRegistries.java
+++ src/main/java/net/newdodgeram/haydensmod/util/ModRegistries.java
@@ -1,16 +1,39 @@
 package net.newdodgeram.haydensmod.util;
 
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
+import net.fabricmc.fabric.api.registry.StrippableBlockRegistry;
+import net.newdodgeram.haydensmod.block.ModBlocks;
 import net.newdodgeram.haydensmod.entity.ModEntities;
 import net.newdodgeram.haydensmod.entity.custom.MountainGolemEntity;
 
 public class ModRegistries {
 
     public static void registerAdditionals() {
+
         registerAttributes();
+        registerFlammableBlock();
+        registerStrippables();
+
     }
 
     private static void registerAttributes() {
         FabricDefaultAttributeRegistry.register(ModEntities.MOUNTAIN_GOLEM, MountainGolemEntity.setAttributes());
+    }
+
+    private static void registerStrippables() {
+        StrippableBlockRegistry.register(ModBlocks.WYRMWOOD_LOG, ModBlocks.STRIPPED_WYRMWOOD_LOG);
+        StrippableBlockRegistry.register(ModBlocks.WYRMWOOD_WOOD, ModBlocks.STRIPPED_WYRMWOOD_WOOD);
+    }
+
+    private static void registerFlammableBlock() {
+        FlammableBlockRegistry instance = FlammableBlockRegistry.getDefaultInstance();
+
+        instance.add(ModBlocks.WYRMWOOD_LOG, 5, 5);
+        instance.add(ModBlocks.WYRMWOOD_WOOD, 5, 5);
+        instance.add(ModBlocks.STRIPPED_WYRMWOOD_LOG, 5, 5);
+        instance.add(ModBlocks.STRIPPED_WYRMWOOD_WOOD, 5, 5);
+        instance.add(ModBlocks.WYRMWOOD_PLANKS, 5, 20);
+
     }
 }
